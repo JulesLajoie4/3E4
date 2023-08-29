2b2cbb81528242a7ea1a93b404f77b7553cb7d9f
Jules Lajoie <2232389@cstj.qc.ca>
2023-08-29 11:30:28 -0400

    Ma première route

--- Semaine 2 rest/server.js
+++ Semaine 2 rest/server.js
@@ -2,4 +2,20 @@
 //server.js => fichier de démarrage => équivalent au main
 import chalk from "chalk";
 
+import app from "./src/app.js";
+
+const PORT = 3000;
+
+app.listen(PORT, err => {
+
+    // Nous avons une erreur
+    if(err){
+        console.log(chalk.red(err));
+        process.exit(1);
+    }
+
+    console.log(chalk.green(`❤️‍🔥 Serveur en fonction sur le port ${PORT} 💯`));
+
+});
+
 console.log(chalk.bgRedBright.whiteBright("Bonjour en couleur 💤"));
