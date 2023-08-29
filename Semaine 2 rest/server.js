c2a48816707e67ad0d1706ea4b476f995dacae11
Jules Lajoie <2232389@cstj.qc.ca>
2023-08-29 10:47:12 -0400

    Début de ma structure de projet

---
+++ Semaine 2 rest/server.js
@@ -0,0 +1,5 @@
+// const chalk = require('chalk'); Ancienne méthode d'import de bibliothèque
+//server.js => fichier de démarrage => équivalent au main
+import chalk from "chalk";
+
+console.log(chalk.bgRedBright.whiteBright("Bonjour en couleur 💤"));
