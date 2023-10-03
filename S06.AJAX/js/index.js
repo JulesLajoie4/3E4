16e0b83b28f603932575c94054780c3990c5df90
Jules Lajoie <2232389@cstj.qc.ca>
2023-10-03 11:20:27 -0400

    avant dernier cours avant semaine relache

--- S06.AJAX/js/index.js
+++ S06.AJAX/js/index.js
@@ -8,9 +8,29 @@ $(document).ready(() => {
 async function retrieveAllPlanet(){
     try{
         const response = await axios.get(SERVICE_URL);
+        if (response.status === 200){
+            //OK
+            const planets = response.data;
+            displayPlanets(planets);
+        }
         console.log(response);
     }
     catch(err){
         console.log(err);
     }
+}
+
+function displayPlanets(planets){
+    planets.forEach(p => {
+        $('#planets').append(displayPlanet(p));
+    });
+}
+
+function displayPlanet(planet){
+    let htmlPlanet = '<div class="col-2 card m-2">';
+    htmlPlanet += `<img src="${planet.icon}" alt="Image de la planète ${planet.name}">`;
+    htmlPlanet += `<a href="details.html?planet=${planet.href}"><h5 class="card-title">${planet.name}</h5></a>`;
+    htmlPlanet += '</div>';
+
+    return htmlPlanet;
 }
